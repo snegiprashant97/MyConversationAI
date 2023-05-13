bfbbb1cb8b0ebd3c408211b0c14bddcaad1eb934
Prashant Singh Negi <lorem@Prashants-Mac-mini.local>
2023-05-14 00:11:42 +0530

    removed config.py from git cache

--- configuration/config.py
+++
@@ -1,6 +0,0 @@
-class Configuration:
-    BASE_PATH = '/Users/lorem/Documents/projects/MyConversationAI/'
-    SPEECH_SAVE_PATH = f'{BASE_PATH}'
-    SPEECH_FILE_NAME = 'converted_speech.mp3'
-
-    AUDIO_RUNNER = '/opt/homebrew/bin/mpg321'
